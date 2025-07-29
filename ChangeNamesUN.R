99c2839a41844b4f1dae28fe054fca117d579052
Andrew Bertoli <abertoli@dartmouth.edu>
2025-07-29 19:13:15 +0200

    Add files via upload

---
+++ ChangeNamesUN.R
@@ -0,0 +1,190 @@
+dat$i[dat$i==12]<-"Algeria"
+dat$j[dat$j==12]<-"Algeria"
+
+
+
+dat$i[dat$i==24]<-"Angola"
+dat$j[dat$j==24]<-"Angola"
+
+dat$i[dat$i==32]<-"Argentina"
+dat$j[dat$j==32]<-"Argentina"
+
+dat$i[dat$i==36]<-"Australia"
+dat$j[dat$j==36]<-"Australia"
+
+dat$i[dat$i==40]<-"Austria"
+dat$j[dat$j==40]<-"Austria"
+
+dat$i[dat$i==56]<-"Belgium"
+dat$j[dat$j==56]<-"Belgium"
+
+dat$i[dat$i==70]<-"Bosnia-Herzegovina"
+dat$j[dat$j==70]<-"Bosnia-Herzegovina"
+
+dat$i[dat$i==76]<-"Brazil"
+dat$j[dat$j==76]<-"Brazil"
+
+dat$i[dat$i==100]<-"Bulgaria"
+dat$j[dat$j==100]<-"Bulgaria"
+
+dat$i[dat$i==120]<-"Cameroon"
+dat$j[dat$j==120]<-"Cameroon"
+
+dat$i[dat$i==152]<-"Chile"
+dat$j[dat$j==152]<-"Chile"
+
+dat$i[dat$i==156]<-"China"
+dat$j[dat$j==156]<-"China"
+
+dat$i[dat$i==170]<-"Colombia"
+dat$j[dat$j==170]<-"Colombia"
+
+dat$i[dat$i==188]<-"Costa Rica"
+dat$j[dat$j==188]<-"Costa Rica"
+
+dat$i[dat$i==191]<-"Croatia"
+dat$j[dat$j==191]<-"Croatia"
+
+dat$i[dat$i==203]<-"Czech Republic"
+dat$j[dat$j==203]<-"Czech Republic"
+
+dat$i[dat$i==208]<-"Denmark"
+dat$j[dat$j==208]<-"Denmark"
+
+dat$i[dat$i==218]<-"Ecuador"
+dat$j[dat$j==218]<-"Ecuador"
+
+dat$i[dat$i==818]<-"Egypt"
+dat$j[dat$j==818]<-"Egypt"
+
+dat$i[dat$i==251]<-"France"
+dat$j[dat$j==251]<-"France"
+
+dat$i[dat$i==276]<-"Germany"
+dat$j[dat$j==276]<-"Germany"
+
+dat$i[dat$i==288]<-"Ghana"
+dat$j[dat$j==288]<-"Ghana"
+
+dat$i[dat$i==300]<-"Greece"
+dat$j[dat$j==300]<-"Greece"
+
+dat$i[dat$i==340]<-"Honduras"
+dat$j[dat$j==340]<-"Honduras"
+
+dat$i[dat$i==352]<-"Iceland"
+dat$j[dat$j==352]<-"Iceland"
+
+dat$i[dat$i==364]<-"Iran"
+dat$j[dat$j==364]<-"Iran"
+
+dat$i[dat$i==372]<-"Ireland"
+dat$j[dat$j==372]<-"Ireland"
+
+dat$i[dat$i==380]<-"Italy"
+dat$j[dat$j==380]<-"Italy"
+
+dat$i[dat$i==384]<-"Ivory Coast"
+dat$j[dat$j==384]<-"Ivory Coast"
+
+dat$i[dat$i==388]<-"Jamaica"
+dat$j[dat$j==388]<-"Jamaica"
+
+dat$i[dat$i==392]<-"Japan"
+dat$j[dat$j==392]<-"Japan"
+
+dat$i[dat$i==484]<-"Mexico"
+dat$j[dat$j==484]<-"Mexico"
+
+dat$i[dat$i==504]<-"Morocco"
+dat$j[dat$j==504]<-"Morocco"
+
+dat$i[dat$i==528]<-"Netherlands"
+dat$j[dat$j==528]<-"Netherlands"
+
+dat$i[dat$i==554]<-"New Zealand"
+dat$j[dat$j==554]<-"New Zealand"
+
+dat$i[dat$i==566]<-"Nigeria"
+dat$j[dat$j==566]<-"Nigeria"
+
+dat$i[dat$i==408]<-"North Korea"
+dat$j[dat$j==408]<-"North Korea"
+
+dat$i[dat$i==579]<-"Norway"
+dat$j[dat$j==579]<-"Norway"
+
+dat$i[dat$i==591]<-"Panama"
+dat$j[dat$j==591]<-"Panama"
+
+dat$i[dat$i==600]<-"Paraguay"
+dat$j[dat$j==600]<-"Paraguay"
+
+dat$i[dat$i==604]<-"Peru"
+dat$j[dat$j==604]<-"Peru"
+
+dat$i[dat$i==616]<-"Poland"
+dat$j[dat$j==616]<-"Poland"
+
+dat$i[dat$i==620]<-"Portugal"
+dat$j[dat$j==620]<-"Portugal"
+
+dat$i[dat$i==642]<-"Romania"
+dat$j[dat$j==642]<-"Romania"
+
+dat$i[dat$i==643]<-"Russia"
+dat$j[dat$j==643]<-"Russia"
+
+dat$i[dat$i==682]<-"Saudi Arabia"
+dat$j[dat$j==682]<-"Saudi Arabia"
+
+dat$i[dat$i==686]<-"Senegal"
+dat$j[dat$j==686]<-"Senegal"
+
+dat$i[dat$i==703]<-"Slovakia"
+dat$j[dat$j==703]<-"Slovakia"
+
+dat$i[dat$i==705]<-"Slovenia"
+dat$j[dat$j==705]<-"Slovenia"
+
+dat$i[dat$i==710]<-"South Africa"
+dat$j[dat$j==710]<-"South Africa"
+
+dat$i[dat$i==410]<-"South Korea"
+dat$j[dat$j==410]<-"South Korea"
+
+dat$i[dat$i==724]<-"Spain"
+dat$j[dat$j==724]<-"Spain"
+
+dat$i[dat$i==752]<-"Sweden"
+dat$j[dat$j==752]<-"Sweden"
+
+dat$i[dat$i==757]<-"Switzerland"
+dat$j[dat$j==757]<-"Switzerland"
+
+dat$i[dat$i==768]<-"Togo"
+dat$j[dat$j==768]<-"Togo"
+
+dat$i[dat$i==780]<-"Trinidad-Tobago"
+dat$j[dat$j==780]<-"Trinidad-Tobago"
+
+dat$i[dat$i==788]<-"Tunisia"
+dat$j[dat$j==788]<-"Tunisia"
+
+dat$i[dat$i==792]<-"Turkey"
+dat$j[dat$j==792]<-"Turkey"
+
+dat$i[dat$i==804]<-"Ukraine"
+dat$j[dat$j==804]<-"Ukraine"
+
+dat$i[dat$i==826]<-"United Kingdom"
+dat$j[dat$j==826]<-"United Kingdom"
+
+dat$i[dat$i==842]<-"United States"
+dat$j[dat$j==842]<-"United States"
+
+dat$i[dat$i==858]<-"Uruguay"
+dat$j[dat$j==858]<-"Uruguay"
+
+dat$i[dat$i==688]<-"Yugoslavia"
+dat$j[dat$j==688]<-"Yugoslavia"
